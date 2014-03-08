36eb882f552a4d9790ead3dd15b6a07de25daf16
Robert L. Wang <rwang6@ucmerced.edu>
2014-03-08 10:17:00 -0800

    Sketching out Chord and Measure classes

--- src/Chord.java
+++ src/Chord.java
@@ -1,8 +1,64 @@
+import java.util.ArrayList;
+
+
 
 public class Chord {
-	private Note[] notes;
+	private ArrayList<Note> noteList;
 	
-	public void addNote() {
-		
+	/**
+	 * Chord() is a constructor that creates a chord object which contains a list of noteList in the given chord.
+	 */
+	public Chord() {
+
+	}
+	
+	/**
+	 * Adds a note to the chord if an existing note doesn't have the same frequency
+	 * @param frequency of the note
+	 * @param duration of the note in milliseconds
+	 * @param sampleRate of the note for playback
+	 */
+	public void addNote(int frequency, int duration, int sampleRate) {
+		for(Note n : noteList) {
+			if(n.getFrequency() == frequency) {
+				//This frequency already exists
+				return;
+			}
+		}
+		Note newNote = new Note(frequency, duration, sampleRate);
+		noteList.add(newNote);
+	}
+	
+	/**
+	 * Deletes the note from the note list 
+	 * @param targetFrequency 
+	 */
+	public void deleteNote(int targetFrequency) {
+		for(Note n : noteList) {
+			if(n.getFrequency() == targetFrequency) {
+				noteList.remove(n);
+				break;
+			}
+		}
+	}
+	
+	/**
+	 * @param targetFrequency of the note
+	 * @return note with the target frequency or null if it wasn't found
+	 */
+	public Note getNote(int targetFrequency) {
+		for(Note n : noteList) {
+			if(n.getFrequency() == targetFrequency) {
+				return n;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * @return number of noteList in the chord
+	 */
+	public int getNumbernoteList() {
+		return noteList.size();
 	}
 }
--- src/Measure.java
+++ src/Measure.java
@@ -1,4 +1,21 @@
+import java.util.ArrayList;
 
 public class Measure {
-
+	private int chordMap;
+	private ArrayList<Chord> chordList;
+	
+	public Measure() {
+		chordMap = 0;
+	}
+	
+	/**
+	 * Adds a chord to the measure at the specified division. 
+	 * Measures are divided into 32 segments as to allow for up to 32nd notes
+	 * @param startPoint falls between 0 and 31, inclusive.
+	 */
+	
+	public void addChord(int startPoint) {
+		assert startPoint > -1 && startPoint < 32;
+		
+	}
 }
