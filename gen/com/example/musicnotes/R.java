afab6752467a2fd0c854a5eedf11a677a066518f
connorzawa <ckanazawa@ucmerced.edu>
2014-04-05 12:13:18 -0700

    Connor: Implemented changing Activities. No errors

--- gen/com/example/musicnotes/R.java
+++ gen/com/example/musicnotes/R.java
@@ -28,16 +28,16 @@ public final class R {
         public static final int treble=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int horizontalScrollView1=0x7f080005;
-        public static final int imageButton1=0x7f080001;
-        public static final int imageButton2=0x7f080002;
-        public static final int imageView1=0x7f080004;
-        public static final int linearLayout1=0x7f080000;
-        public static final int linearLayout2=0x7f080003;
+        public static final int action_settings=0x7f080005;
+        public static final int imageView1=0x7f080002;
+        public static final int keySignatureLayout=0x7f080001;
+        public static final int measureScrollView=0x7f080004;
+        public static final int notesToolBar=0x7f080000;
+        public static final int timeSignatureLayout=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int editor_layout=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -47,6 +47,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int button_send=0x7f050002;
         public static final int edit_message=0x7f050001;
+        public static final int editor_name=0x7f050005;
         public static final int title_activity_main=0x7f050004;
     }
     public static final class style {
