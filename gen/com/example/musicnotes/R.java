5401a9cff8f38bb9a12b876cb7bd29c36d1ed5bc
connorzawa <ckanazawa@ucmerced.edu>
2014-04-05 15:36:30 -0700

    Connor: Changes to Editor layout

--- gen/com/example/musicnotes/R.java
+++ gen/com/example/musicnotes/R.java
@@ -21,19 +21,28 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int fillednote=0x7f020000;
-        public static final int fillednotenote=0x7f020001;
-        public static final int halfnote=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int treble=0x7f020004;
+        public static final int csignature=0x7f020000;
+        public static final int fillednote=0x7f020001;
+        public static final int fillednoteline=0x7f020002;
+        public static final int fillednotenote=0x7f020003;
+        public static final int fillednotespace=0x7f020004;
+        public static final int four=0x7f020005;
+        public static final int halfnote=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int line=0x7f020008;
+        public static final int treble=0x7f020009;
     }
     public static final class id {
-        public static final int imageView1=0x7f080002;
-        public static final int keySignatureLayout=0x7f080001;
-        public static final int main_menu=0x7f080005;
-        public static final int measureScrollView=0x7f080004;
+        public static final int keySignatureLayout=0x7f080006;
+        public static final int keysignaturemeasure=0x7f080007;
+        public static final int main_menu=0x7f080009;
+        public static final int measureScrollView=0x7f080008;
         public static final int notesToolBar=0x7f080000;
-        public static final int timeSignatureLayout=0x7f080003;
+        public static final int staffclef=0x7f080002;
+        public static final int timeSignatureLayout=0x7f080001;
+        public static final int timesigbot=0x7f080004;
+        public static final int timesignaturemeasure=0x7f080005;
+        public static final int timesigtop=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -50,6 +59,7 @@ public final class R {
         public static final int edit_message=0x7f050001;
         public static final int editor_name=0x7f050005;
         public static final int mainMenu_name=0x7f050006;
+        public static final int timesigtop_file_path=0x7f050007;
         public static final int title_activity_main=0x7f050004;
     }
     public static final class style {
--- src/com/example/musicnotes/EditorActivity.java
+++ src/com/example/musicnotes/EditorActivity.java
@@ -1,20 +1,28 @@
 package com.example.musicnotes;
 
+import MusicSheet.Sheet;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.*;
 
 public class EditorActivity extends Activity{
 
-
+	Sheet sheet;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
       
     	super.onCreate(savedInstanceState);
     	setContentView(R.layout.editor_layout);
+    	ImageView timeSigTop = (ImageView) findViewById(R.id.timesigtop);
+    	timeSigTop.setImageResource(R.drawable.halfnote);
+    	sheet = new Sheet();
+    	//Add Buttons to notesToolBar
     }
     
 
@@ -36,5 +44,21 @@ public class EditorActivity extends Activity{
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+    
+    public void updateToolBar()
+    {
+    	LinearLayout toolbar = (LinearLayout) findViewById(R.id.notesToolBar);
+    	ImageButton[] toolbarButtons = new ImageButton[6];
+    	for(int i = 0; i < toolbarButtons.length; i++)
+    	{
+    		toolbarButtons[i] = new ImageButton(this);
+    		toolbarButtons[i].setImageResource(R.drawable.four);
+    		toolbarButtons[i].setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT));
+    		toolbar.addView(toolbarButtons[i]);
+    		setContentView(toolbar);
+    	}
+
+    	
     }
 }
