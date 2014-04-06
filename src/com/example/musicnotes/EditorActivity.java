ba922e92bb9638dac6cc64c00b4e6d682a59c312
connorzawa <ckanazawa@ucmerced.edu>
2014-04-06 10:36:03 -0700

    Connor: Added Spinner to change values of tools

--- src/com/example/musicnotes/EditorActivity.java
+++ src/com/example/musicnotes/EditorActivity.java
@@ -10,35 +10,59 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.*;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class EditorActivity extends Activity{
 
 	Sheet sheet;
-	
-    @Override
+	Spinner spinner;
+	enum EditorVal{NOTES, RESTS, ACCIDENTALS};
+	EditorVal currentVal;
+    
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
       
     	super.onCreate(savedInstanceState);
     	setContentView(R.layout.editor_layout);
     	ImageView timeSigTop = (ImageView) findViewById(R.id.timesigtop);
     	timeSigTop.setImageResource(R.drawable.halfnote);
-
     	
-    	Spinner spinner = (Spinner) findViewById(R.id.toolbarSpinner);
-    	ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-    	        R.array.toolbarSpinnerArray, android.R.layout.simple_spinner_item);
+    	currentVal = EditorVal.NOTES;
+    	spinner = (Spinner) findViewById(R.id.toolbarSpinner);
+    	
+    	ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.toolbarSpinnerArray, android.R.layout.simple_spinner_item);
     	adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
     	spinner.setAdapter(adapter);
     	
+    	spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+    	    @Override
+    	    public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+    	        if(position == 0)
+    	        {
+    	        	currentVal = EditorVal.NOTES;
+    	        }
+    	        else if(position == 1)
+    	        {
+    	        	currentVal = EditorVal.RESTS;
+    	        }
+    	        else
+    	        {
+    	        	currentVal = EditorVal.ACCIDENTALS;
+    	        }
+    	        updateToolBar();
+    	    }
+
+    	    @Override
+    	    public void onNothingSelected(AdapterView<?> parentView) {
+    	      
+    	    }
+
+    	});
     	updateToolBar();
     	
     	
     	sheet = new Sheet();
 
-    	//Add Buttons to notesToolBar
-    	
-    	//Add Spinner Options
-    	
     }
 
     @Override
@@ -67,25 +91,22 @@ public class EditorActivity extends Activity{
     	LinearLayout toolbar = (LinearLayout) findViewById(R.id.notesToolBar);
     	// Make sure there is nothing in there
     	toolbar.removeAllViews();
-    	
-     	Spinner spinner = (Spinner) findViewById(R.id.toolbarSpinner);
-     	int selectedItem = spinner.getSelectedItemPosition();
-    	
-    	ImageButton[] toolbarButtons = new ImageButton[6];
+   
+    	ImageButton[] toolbarButtons = new ImageButton[2];
     	for(int i = 0; i < toolbarButtons.length; i++)
     	{
     		toolbarButtons[i] = new ImageButton(this);
-    		if(selectedItem == 1)
+    		switch(currentVal)
     		{
-    			toolbarButtons[i].setImageResource(R.drawable.fillednote);
-    		}
-    		else if(selectedItem == 2)
-    		{
-    			toolbarButtons[i].setImageResource(R.drawable.halfnote);
-    		}
-    		else 
-    		{
-    			toolbarButtons[i].setImageResource(R.drawable.four);
+    			case NOTES:
+    		  			toolbarButtons[i].setImageResource(R.drawable.fillednote);
+    		  			break;
+    			case RESTS:
+    		  			toolbarButtons[i].setImageResource(R.drawable.halfnote);
+    		  			break;
+    			case ACCIDENTALS:
+    				toolbarButtons[i].setImageResource(R.drawable.four);
+    				break;
     		}
     		toolbarButtons[i].setLayoutParams(new LayoutParams(100, LayoutParams.MATCH_PARENT));
     		toolbar.addView(toolbarButtons[i]);
